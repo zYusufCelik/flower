fc0eb91d8add9438ad94b666d85cb49480736b75
yağmur çelik <yagmurcelik01@gmail.com>
2025-03-25 21:40:30 +0300

    Add backend project files

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+using backend.Models;
+
+namespace backend.Data;
+
+public partial class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Chart> Charts { get; set; }
+
+    public virtual DbSet<Process> Processes { get; set; }
+
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//         => optionsBuilder.UseMySql("server=aws-db.c1wgu02mewc9.eu-north-1.rds.amazonaws.com;database=Flower;user=admin;password=my-secret-pw;port=3306", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Chart>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.Property(e => e.Name).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<Process>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.HasIndex(e => e.ChartId, "ChartId");
+
+            entity.Property(e => e.Distance).HasPrecision(10, 2);
+            entity.Property(e => e.ProcessName).HasMaxLength(255);
+            entity.Property(e => e.Shape).HasMaxLength(100);
+            entity.Property(e => e.Time).HasPrecision(10, 2);
+
+            entity.HasOne(d => d.Chart).WithMany(p => p.Processes)
+                .HasForeignKey(d => d.ChartId)
+                .HasConstraintName("Processes_ibfk_1");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ backend/Models/Chart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models;
+
+public partial class Chart
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Process> Processes { get; set; } = new List<Process>();
+}
